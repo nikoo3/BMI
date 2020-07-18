ea334118f51eb72bbbd4c77ae99fb0d0b8552e3f
Mateusz <mnikel93@gmail.com>
2020-07-18 11:13:55 +0200

    Zadanie kompletne

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swoją wage: ");
+        float waga = scanner.nextFloat();
+        System.out.println("Podaj swoj wzrost w metrach: ");
+        float wzrost = scanner.nextFloat();
+        float BMI = waga/(wzrost*wzrost);
+        System.out.println(BMI);
+        System.out.println(BMI > 18.5 || BMI < 24.9 ? "BMI jest optymalne" : "BMI jest nieoptymalne");
+    }
+}
