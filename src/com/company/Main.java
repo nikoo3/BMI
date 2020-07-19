7873ca6523018c7c0145c441d1154fc32e383d84
Mateusz <mnikel93@gmail.com>
2020-07-19 10:36:04 +0200

    BMI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,17 @@
 package com.company;
-
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Podaj swoją wage: ");
         float waga = scanner.nextFloat();
+
         System.out.println("Podaj swoj wzrost: ");
         int wzrost = scanner.nextInt()/100;
+
         float BMI = waga/(wzrost * wzrost);
         System.out.println(BMI);
-        System.out.println(BMI > 18.5 || BMI < 24.9 ? "BMI jest optymalne" : "BMI jest nieoptymalne");
-    }
-}
+        System.out.println(BMI > 18.5 || BMI < 24.9 ? "BMI jest optymalne" : "BMI jest nieoptymalne"); }}
